4c32381405037561562350fdf5d89aab86bad788
vinay <vinayvarma2545@gmail.com>
2022-08-24 17:29:15 +0530

    createElement

---
+++ createElement.js
@@ -0,0 +1,21 @@
+// document.getElementById("lol")
+// // Specify the string from which 
+//     // the elements would be created
+//     var str = "h2";
+//     var str2 = "p";
+  
+//     // Creating the elements 
+//     var elem =
+//         document.createElement(str);
+//     var elem2 =
+//         document.createElement(str2);
+  
+//     // Insert text in the element
+//     elem.innerText =
+//       "This is the new heading element";
+//     elem2.innerText =
+//       "This is the new paragraph element";
+  
+//     // Add the element to the body 
+//     document.body.appendChild(elem);
+//     document.body.appendChild(elem2);
